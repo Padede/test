ea92d298c2ae6a55e059570e97ccec8f24650ce1
Bogdan Potulskiy <avyf23@mail.ru>
2024-03-22 16:35:32 +0300

    qq

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
 
 export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
     fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
