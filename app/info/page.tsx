a53a6693fe73768998ec6791b8d7c5050b1b498e
mira <miraworkemail@gmail.com>
2024-04-03 17:23:16 +0300

    create new page

---
+++ app/info/page.tsx
@@ -0,0 +1,11 @@
+function Info() {
+  return (
+    <main className="overflow-hidden pt-36">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla labore
+      deserunt tempore beatae dolorem illo hic, ducimus delectus veniam pariatur
+      neque. Minima culpa a assumenda amet accusantium corrupti, nam molestias.
+    </main>
+  );
+}
+
+export default Info;
